0c42671cba4f757b3f0a0bc33b2b929135c4ec47
TTTTina33487 <tinatang20241127@gmail.com>
2026-05-14 17:41:37 +0800

    Update mlp.py and export new mlp builders

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,12 +1,14 @@
 """Model implementations for Assignment 2."""
 
 from src.models.cnn import ManualCNNClassifier, build_cnn, build_cnn_8x64, build_cnn_32x16
-from src.models.mlp import MLPClassifier, build_mlp
+from src.models.mlp import MLPClassifier, build_mlp, build_mlp_deep, build_mlp_wide
 
 __all__ = [
     "MLPClassifier",
     "ManualCNNClassifier",
     "build_mlp",
+    "build_mlp_deep",
+    "build_mlp_wide",
     "build_cnn",
     "build_cnn_32x16",
     "build_cnn_8x64",
